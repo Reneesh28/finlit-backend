17586955603705792533966db9bcc7350118e254
Reneesh28 <reneesh3508925@gmail.com>
2026-03-26 12:50:16 +0530

    PHASE 1

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+connectDB();
+
+
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
